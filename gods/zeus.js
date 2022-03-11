57b4a86216786c3d2f73b7ea3f6f7695738a788f
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2022-03-11 14:19:31 +1300

    generate zeus combinations

--- gods/zeus.js
+++ gods/zeus.js
@@ -1,4 +1,6 @@
-const zeusCombination = {
+const generateNumber = require('generateNumber')
+
+const zeusCombinations = {
   background: 0,
   skinTone: 0,
   weaponName: 0,
@@ -14,4 +16,44 @@ const zeusCombination = {
 const zeusPathToStoreFinal = './final-images/zeus-final'
 const zeusPathToImages = './images/ZEUS'
 const zeusLayers = 10
+const maxCountRareBackgrounds = 4
+const rareTraitCount = 20
+const rareImagesBackground = 5
+const rareBackgroundCount = 20
+const normalBackgroundOptions = 6
+const normalVariationOptions = 5
 // const rareTraitsAllocation = 2
+
+function generateZeusCombinations (zeusCombinations) {
+  const combinationCount = 20
+  const zeusCombinations = []
+  for (let i = 0; i < combinationCount; i++) {
+    const thisCombo = {
+      background: generateNumber(normalBackgroundOptions),
+      skinTone: generateNumber(normalVariationOptions),
+      weaponName: generateNumber(normalVariationOptions),
+      weaponColour: generateNumber(normalVariationOptions),
+      hairColour: generateNumber(normalVariationOptions),
+      hairStyle: generateNumber(normalVariationOptions),
+      beardStyle: generateNumber(normalVariationOptions),
+      armourName: generateNumber(normalVariationOptions),
+      armourColour: generateNumber(normalVariationOptions),
+      eyeMarkings: generateNumber(normalVariationOptions)
+    }
+    const checkComboExists = checkIfZeusCombinationExists(zeusCombinations, thisCombo)
+    if (checkComboExists === 'not exists') {
+      zeusCombinations.push(thisCombo)
+    } else if (checkComboExists === 'exists') {
+      console.log('Duplicate found and discarded')
+    }
+  }
+}
+
+function checkIfZeusCombinationExists (zeusCombinations, thisCombo) {
+  for (let i = 0; i < zeusCombinations.length; i++) {
+    if (zeusCombinations[i] === thisCombo) {
+      return 'exists'
+    }
+  }
+  return 'not exists'
+}
