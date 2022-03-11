592341b86bc1d752742d4b629b7699bed4ab0c48
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2022-03-11 13:06:44 +1300

    variables for zeus logic

---
+++ gods/zeus.js
@@ -0,0 +1,17 @@
+const zeusCombination = {
+  background: 0,
+  skinTone: 0,
+  weaponName: 0,
+  weaponColour: 0,
+  hairColour: 0,
+  hairStyle: 0,
+  beardStyle: 0,
+  armourName: 0,
+  armourColour: 0,
+  eyeMarkings: 0
+}
+
+const zeusPathToStoreFinal = './final-images/zeus-final'
+const zeusPathToImages = './images/ZEUS'
+const zeusLayers = 10
+// const rareTraitsAllocation = 2
