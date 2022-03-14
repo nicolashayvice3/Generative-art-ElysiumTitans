9e5aed3bf3ee89ff1e188c6314645d9429456345
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2022-03-14 20:30:07 +1300

    initial zeus test planed out

--- tests/zeus.test.js
+++ tests/zeus.test.js
@@ -1,10 +1,23 @@
 const zeus = require('../gods/zeus')
 
-const combinationCount = 20
+const combinationCount = 1000
 
 describe('generateZeusCombinations', () => {
   test('max 20 combinations', () => {
     const actual = zeus.generateZeusCombinations(combinationCount)
-    expect(actual).toHaveLength(20)
+    expect(actual).toHaveLength(1000)
   })
+  test('pushes an extra combination to combinations array', () => {
+    const expected = 1
+    const actual = 1
+    expect(expected).not.toContain(actual)
+  })
+})
+
+test('generateNumber creates a random number between the selected parameter and 0', () => {
+  expect(1).toBe(1)
+})
+
+test('checkIfZeusCombinationExists returns "exists" if existing combination is present', () => {
+  expect(2).not.toBe(1)
 })
