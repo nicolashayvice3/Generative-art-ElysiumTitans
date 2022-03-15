1d13eb0223ec35231629fccd53b9a8ed8a217f0b
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2022-03-16 12:07:06 +1300

    checkIfZeusCombinationsExist test written

--- gods/zeus.js
+++ gods/zeus.js
@@ -53,5 +53,6 @@ function checkIfZeusCombinationExists (combinations, thisCombo) {
 
 module.exports = {
   generateZeusCombinations,
-  generateNumber
+  generateNumber,
+  checkIfZeusCombinationExists
 }
--- tests/zeus.test.js
+++ tests/zeus.test.js
@@ -1,27 +1,73 @@
 const zeus = require('../gods/zeus')
 
 const combinationCount = 30
-const number = 5
+const number = 10
+const thisCombo = {
+  background: zeus.generateNumber(5),
+  skinTone: zeus.generateNumber(number),
+  weaponName: zeus.generateNumber(number),
+  weaponColour: zeus.generateNumber(number),
+  hairColour: zeus.generateNumber(number),
+  hairStyle: zeus.generateNumber(number),
+  beardStyle: zeus.generateNumber(number),
+  armourName: zeus.generateNumber(number),
+  armourColour: zeus.generateNumber(number),
+  eyeMarkings: zeus.generateNumber(number)
+}
+const combinations = {
+  1: {
+    background: zeus.generateNumber(5),
+    skinTone: zeus.generateNumber(number),
+    weaponName: zeus.generateNumber(number),
+    weaponColour: zeus.generateNumber(number),
+    hairColour: zeus.generateNumber(number),
+    hairStyle: zeus.generateNumber(number),
+    beardStyle: zeus.generateNumber(number),
+    armourName: zeus.generateNumber(number),
+    armourColour: zeus.generateNumber(number),
+    eyeMarkings: zeus.generateNumber(number)
+  },
+  2: {
+    background: zeus.generateNumber(5),
+    skinTone: zeus.generateNumber(number),
+    weaponName: zeus.generateNumber(number),
+    weaponColour: zeus.generateNumber(number),
+    hairColour: zeus.generateNumber(number),
+    hairStyle: zeus.generateNumber(number),
+    beardStyle: zeus.generateNumber(number),
+    armourName: zeus.generateNumber(number),
+    armourColour: zeus.generateNumber(number),
+    eyeMarkings: zeus.generateNumber(number)
+  },
+  3: {
+    background: zeus.generateNumber(5),
+    skinTone: zeus.generateNumber(number),
+    weaponName: zeus.generateNumber(number),
+    weaponColour: zeus.generateNumber(number),
+    hairColour: zeus.generateNumber(number),
+    hairStyle: zeus.generateNumber(number),
+    beardStyle: zeus.generateNumber(number),
+    armourName: zeus.generateNumber(number),
+    armourColour: zeus.generateNumber(number),
+    eyeMarkings: zeus.generateNumber(number)
+  }
+}
 
 describe('generateZeusCombinations', () => {
   test('max 30 combinations', () => {
     const actual = zeus.generateZeusCombinations(combinationCount)
     expect(actual).toHaveLength(30)
   })
-  test('pushes an extra combination to combinations array', () => {
-    const expected = 1
-    const actual = 1
-    expect(expected).not.toContain(actual)
-  })
 })
 
 test('generateNumber creates a random number between the selected parameter and 0', () => {
   const actual = zeus.generateNumber(number)
-  console.log(actual)
   expect(actual).not.toBe(0)
-  expect(actual).toBeLessThan(6)
+  expect(actual).toBeLessThan(11)
 })
 
-// test('checkIfZeusCombinationExists returns "exists" if existing combination is present', () => {
-//   expect(2).not.toBe(1)
-// })
+test('checkIfZeusCombinationExists returns "not exists" if existing combination is not present', () => {
+  const actual = zeus.checkIfZeusCombinationExists(combinations, thisCombo)
+  expect(actual).toBe('not exists')
+  expect(combinations).not.toContain(thisCombo)
+})
